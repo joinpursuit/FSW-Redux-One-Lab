3e274a4675fd138b45dfa870a71122c60208b6b0
Marvin Thompson <marvinthompson@pursuit.org>
2020-04-04 15:21:55 -0400

    working on input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,10 @@ import React from "react";
 const Counter = ({ value, onIncrement, onDecrement, onReset }) => (
   <div>
     <p>value: {value}</p>
+    <form>
+        <input type="number"/>
+        <button type="submit">Set yo value, dawg</button>
+    </form>
     <p>
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>

1ecccc02a50de52c8ee9d50e1faa047d6b23a021
trilltown <phil.awich@gmail.com>
2020-04-04 14:15:15 -0400

    Install redux & react-router-dom

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,5 +1,8 @@
 import { INCREMENT, DECREMENT } from "../actions/actionTypes";
 
 export default (state = 0, action) => {
- 
+    switch(action.type) {
+        case INCREMENT:
+            return
+    }
 };

28bb56b2932a148840cf5b55d97b36b8bb643418
karenm09 <karenm929@hotmail.com>
2020-04-04 16:39:40 -0400

    setup the dispatcher to hit the reducer

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Counter from "../components/Counter";
+import { useSelector, useDispatch} from 'react-redux';
 import { incrementCount, decrementCount } from "../actions/counterActions";
 
 const CounterContainer = () => {
-  let count = 0
+  // let count = 0
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const increment = () => {
+    dispatch(incrementCount())
   };
 
   const decrement = () => {
+    dispatch(decrementCount())
   };
 
   return (

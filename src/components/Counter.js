e790a8aa87869b7c40eb4dc1c31186770ac9fd6e
karenm09 <karenm929@hotmail.com>
2020-04-04 19:49:23 -0400

    remove the form

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,9 +4,7 @@ const Counter = ({ value, onIncrement, onDecrement, reset, input }) => (
   <div>
     <p>value: {value}</p>
     <p>
-      <form>
-        <input placeholder="Enter a number" type="number" onChange={input}/>
-      </form>
+      <input placeholder="Enter a number" type="number" onChange={input} />
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
       <button onClick={reset}>Reset</button>

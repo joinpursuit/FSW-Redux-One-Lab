551c6fd5f8489f534b0bb18b5a2bfae714574c93
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-04-04 15:46:54 -0400

    complete MVPs for lab

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux"
 import Counter from "../components/Counter";
-import { incrementCount, decrementCount } from "../actions/counterActions";
+import { incrementCount, resetCount, decrementCount } from "../actions/counterActions";
 
 const CounterContainer = () => {
-  let count = 0
+  const count = useSelector(state => state.count)
+  const dispatch = useDispatch()
+
 
   const increment = () => {
+    dispatch(incrementCount())
   };
 
   const decrement = () => {
+    dispatch(decrementCount())
+  };
+
+  const reset = () => {
+    dispatch(resetCount())
   };
 
   return (
-    <Counter
-      value={count}
-      onIncrement={increment}
-      onDecrement={decrement}
-    />
+    <React.Fragment>
+      <Counter
+        value={count}
+        onIncrement={increment}
+        onReset={reset}
+        onDecrement={decrement}
+      />
+    </React.Fragment>
   )
 }
 

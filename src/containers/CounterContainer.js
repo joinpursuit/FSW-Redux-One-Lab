2fd9b3268ff47185c6185c8c16e16bf3bab59bde
trilltown <phil.awich@gmail.com>
2020-04-04 17:17:17 -0400

    Update CounterContainter

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Counter from "../components/Counter";
+import { useSelector, useDispatch } from 'react-redux'
 import { incrementCount, decrementCount } from "../actions/counterActions";
 
 const CounterContainer = () => {
-  let count = 0
+  // let count = 0;
+  const count = useSelector(state => state.count)
+  console.log("count ", count);
+  
+  const dispatch = useDispatch();
 
   const increment = () => {
+    dispatch(incrementCount())
   };
 
   const decrement = () => {
+    dispatch(decrementCount())
   };
 
   return (
-    <Counter
-      value={count}
-      onIncrement={increment}
-      onDecrement={decrement}
+    <Counter value={count} onIncrement={increment} onDecrement={decrement}
     />
   )
 }

155f7711a10ce74bfb190d6790b8db174344e06a
ashya <ashyamanning@pursuit.org>
2020-04-05 15:18:06 -0400

    update state in CountContainer

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,5 +1,14 @@
 import { INCREMENT, DECREMENT } from "../actions/actionTypes";
 
-export default (state = 0, action) => {
- 
+const CountReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
 };
+
+export default CountReducer;
